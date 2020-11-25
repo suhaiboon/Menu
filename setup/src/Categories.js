bbc5d0c4668893181413dd9e06b690b5313355f5
Suhaiboon <suhaibtareq@gmail.com>
2020-11-25 12:31:59 -0500

    Finished 5th RP

--- setup/src/Categories.js
+++ setup/src/Categories.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            className="filter-btn"
+            onClick={() => filterItems(category)}
+            key={index}
+          >
+            {category}
+          </button>
+        );
+      })}
+      {/* <button className="filter-btn" onClick={() => filterItems("all")}>
+        All
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
+        Breakfast
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("lunch")}>
+        Lunch
+      </button>
+      <button className="filter-btn" onClick={() => filterItems("shakes")}>
+        Shakes
+      </button> */}
+    </div>
+  );
 };
 
 export default Categories;
